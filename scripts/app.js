316a44902861fa98a114ffc4a0d6a8faa4110709
Jonas <jonasdenzel@web.de>
2020-12-10 09:28:40 +0100

    test

--- scripts/app.js
+++ scripts/app.js
@@ -240,3 +240,26 @@ function persistentNotification() {
     alert('Notification API error: ' + err);
   }
 }
+
+//Bilder im Cache 
+function preloadImages(array) {
+  if (!preloadImages.list) {
+      preloadImages.list = [];
+  }
+  var list = preloadImages.list;
+  for (var i = 0; i < array.length; i++) {
+      var img = new Image();
+      img.onload = function() {
+          var index = list.indexOf(this);
+          if (index !== -1) {
+              // remove image from the array once it's loaded
+              // for memory consumption reasons
+              list.splice(index, 1);
+          }
+      }
+      list.push(img);
+      img.src = array[i];
+  }
+}
+
+preloadImages(["url1.jpg", "url2.jpg", "url3.jpg"]);
