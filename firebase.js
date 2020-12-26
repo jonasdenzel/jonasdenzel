b6e593468848b5f7e88d010f1b3e4c8ca3197478
Jonas <jonasdenzel@web.de>
2020-12-26 11:01:13 +0100

    test

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config ={
+    apiKey: "AIzaSyBCawdKz-whpKC3SyaVdW5gYA-dhSifOJ8",
+    authDomain: "pwa-app-c5dce.firebaseapp.com",
+    projectId: "pwa-app-c5dce",
+    storageBucket: "pwa-app-c5dce.appspot.com",
+    messagingSenderId: "1026614224309",
+    appId: "1:1026614224309:web:e71601543c62e3d563a5fc"
+
+}
+firebase.initializeApp(config)
+
+export default firebase
